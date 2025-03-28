44a624d759cb6f0b27daabca2c7e66fb4fad54ae
stormingsilver <155936140+stormingsilver@users.noreply.github.com>
2025-03-27 21:54:45 -0500

    Create mobs.js

---
+++ JavaScript/mobs.js
@@ -0,0 +1,13 @@
+function overworldFunction() {
+    document.getElementById("content_b").innerHTML="Overworld mobs";
+}
+
+function netherFunction() {
+    document.getElementById("content_b").innerHTML="Nether mobs";
+}
+
+function endFunction() {
+    document.getElementById("content_b").innerHTML="End mobs"; //make into string variable
+}
+
+overworldFunction(); /*sets default to overworld*/
