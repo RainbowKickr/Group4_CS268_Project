75af6c2a1592b8f81f7895e29b6ac41517a3474e
stormingsilver <155936140+stormingsilver@users.noreply.github.com>
2025-04-20 23:37:09 -0500

    Mira

---
+++ JavaScript/biomes.js
@@ -0,0 +1,28 @@
+function overworldFunction() {
+    document.getElementById("info").innerHTML= "<br>Plains biomes" +
+    "<br>forest biomes" +
+    "<br>savanna biomes" +
+    "<br>highlands biomes" +
+    "<br>desert biomes" +
+    "<br>swamp & beach biomes" +
+    "<br>Cave biomes" +
+    "<br>ocean biomes";
+}
+
+function netherFunction() {
+    document.getElementById("info").innerHTML= "There are 5 biomes in the Nether, each very unique from the others. The first one to mention is the 'Basalt Deltas'. Made up of mostly its namesake, " +
+    "<br><br>basalt in varying heights of pillers. There is a good amount of blackstone, magma blocks and lava scattered throughtout the basalt deltas." +
+    "<br>crimson forest" +
+    "<br>warped forest" +
+    "<br>nether wastes" +
+    "<br>soul sand vally";
+}
+
+function endFunction() {
+    document.getElementById("info").innerHTML= "There surprisingly are a few biomes in the End. The first one you encounter is called 'The End'. It's the end island you spawn on. The radius" + 
+    "<br>of the end island is roughly 1,000 blocks (meters) and is centered at (0, 0)" +
+    "<br><br>After defeating the ender dragon and teleporting to the further away islands, you will notice there are some very tiny islands clustered around. " +
+    "<br><br>These are the 'Small End Islands'. The larger outer end islands are made up of 3 different biomes each. There's the 'End Barrens' which are the rims of the larger" +
+    "<br><br> islands. There's the 'End Midlands', which is the slope from the hillier parts of a larger island to the edges of it. And finally there's the 'End Highlands', the hilltops" +
+    "<br><br> of the larger islands";
+}
