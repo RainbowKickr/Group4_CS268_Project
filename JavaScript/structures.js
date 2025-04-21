c1089e9c058613a37d51fe0eab9aa454e19c4fa1
stormingsilver <155936140+stormingsilver@users.noreply.github.com>
2025-04-20 23:39:36 -0500

    Mira

---
+++ JavaScript/structures.js
@@ -0,0 +1,25 @@
+function overworldFunction() {
+    document.getElementById("info").innerHTML= "stronghold" +
+    "<br>ancient city" +
+    "<br>pillager outpost" +
+    "<br>village" +
+    "<br>desert temple" +
+    "<br>woodland mansion" +
+    "<br>ocean monument" +
+    "<br>trial chambers" +
+    "<br>trail ruins";
+}
+
+function netherFunction() {
+    document.getElementById("info").innerHTML= "Bastion remnants are found in every nether biome except basalt deltas. They are full of piglins, piglin brutes and the occasional magma cube and hoglin. "+
+    "<br>These structures can have smithing templates, smithing upgrades, ancient debris, ancient debris scrap and a lot of gold." +
+    "<br><br>Nether fortress" +
+    "<br><br>Nether fossil" +
+    "<br><br>ruined portel";
+}
+
+function endFunction() {
+    document.getElementById("info").innerHTML= "There is only 1 structure in the End, End Cities. These cities can have enchanted diamond armor, tools and loose diamonds, emeralds, gold and iron." +
+    "<br> These cities also have shulks, which can drop shulker shells that can be used to create shulker boxes. If there is a floating boat at the end city, " +
+    "<br>then there is an elytra also at the city, which when paired with flight rockets allow you to travel much faster than most modes of transportation.";
+}
