6df958d2bf58bd2d63aed242152a57a6eceaa3f3
YaBoiSheepish <BIESTEEW5161@uwec.edu>
2025-05-04 17:50:26 -0500

    Everything
    My portion of the code

---
+++ JavaScript/Caves.js
@@ -0,0 +1,7 @@
+const background = document.getElementById("scrollBackground");
+    window.addEventListener("scroll", () => {
+        let scrollY = window.scrollY;
+        let newHeight = Math.max(0, 200 - scrollY);
+        background.style.height = `${newHeight}px`;
+    }
+);
