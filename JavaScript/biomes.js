1973726a4197f6cc6c1c7e092e4fa08f3b55f652
stormingsilver <155936140+stormingsilver@users.noreply.github.com>
2025-05-04 17:52:59 -0500

    Finished structures page and touched up mobs page

--- JavaScript/biomes.js
+++ JavaScript/biomes.js
@@ -7,22 +7,36 @@ function overworldFunction() {
     "<br>swamp & beach biomes" +
     "<br>Cave biomes" +
     "<br>ocean biomes";
+    document.getElementById("f").style.height = "auto";
 }
 
 function netherFunction() {
-    document.getElementById("info").innerHTML= "There are 5 biomes in the Nether, each very unique from the others. The first one to mention is the 'Basalt Deltas'. Made up of mostly its namesake, " +
-    "<br><br>basalt in varying heights of pillers. There is a good amount of blackstone, magma blocks and lava scattered throughtout the basalt deltas." +
-    "<br>crimson forest" +
-    "<br>warped forest" +
-    "<br>nether wastes" +
-    "<br>soul sand vally";
+    document.getElementById("info").innerHTML= "<p>There are 5 biomes in the Nether, each very unique from the others. Below each biome are lava lakes that go to bedrock.</p>" +
+    /* Basalt Delta */"<h3>Basalt Delta</h3><img src='/Images/basaltdelta.webp' alt='basalt delta' style=float:left>" +
+    "<p>The first one to mention is the 'Basalt Deltas'. Made up of mostly its namesake, basalt in varying heights of pillers. " +
+    "There is also a good amount of blackstone, magma blocks and lava scattered throughtout the basalt deltas.</p>" +
+    "<p>Quartz ore, neather gold ore and ancient debris all spawn underground in basalt deltas</p>" +
+    "<p>Be wary while traversing through here, attempting to mine directly through the basalt could lead to lava pools. " +
+    "Traveling through these biomes is recommended to be done with an elytra or by placing blocks.</p>" +
+    "<p>Magma cubes spawn more frequently in basalt deltas, striders and ghasts also spawn in basalt deltas. Pigmen, piglins and hoglins don't spawn in basalt deltas.</p>" +
+    "<h3>crimson forest</h3>" +
+    "<h3>warped forest</h3>" +
+    "<h3>nether wastes</h3>" +
+    "<h3>soul sand vally</h3>";
+    document.getElementById("f").style.height = "auto";
 }
 
 function endFunction() {
-    document.getElementById("info").innerHTML= "There surprisingly are a few biomes in the End. The first one you encounter is called 'The End'. It's the end island you spawn on. The radius" + 
-    "<br>of the end island is roughly 1,000 blocks (meters) and is centered at (0, 0)" +
-    "<br><br>After defeating the ender dragon and teleporting to the further away islands, you will notice there are some very tiny islands clustered around. " +
-    "<br><br>These are the 'Small End Islands'. The larger outer end islands are made up of 3 different biomes each. There's the 'End Barrens' which are the rims of the larger" +
-    "<br><br> islands. There's the 'End Midlands', which is the slope from the hillier parts of a larger island to the edges of it. And finally there's the 'End Highlands', the hilltops" +
-    "<br><br> of the larger islands";
+    document.getElementById("info").innerHTML= "<p>There surprisingly are 5 biomes in the End, though all End biomes are made up of floating endstone islands.</p>"+
+    /* End */"<h3>End (the biome)</h3><img src='/Images/endBiome.webp' alt='end' (the biome) style='float:left;margin:0px 15px 15px 15px'>" +
+    "<p>The first one you encounter is called 'The End'. It's the end island a player spawn on. The radius of the end island is roughly 1,000 blocks (meters) and is centered at (0, 0). " +
+    "Large obsidian pillers with end crystals generate in a circle formation around the end portal back to the overworld. " +
+    "The first time a player enters the ender dragon spawns and the portal home is not activated until the ender dragon is defeated.</p>" +
+    /* All other biomes */"</h3><img src='/Images/endAllOtherBiomes.png' alt= 'all other end biomes' style='float:right;margin: 0px 15px 50px 15px'>" +
+    "<h3>Small End Islands, End Midlands, End Highlands, End Barrens</h3><p>After using the end gateway to teleport to the further away islands, you will notice there are some very tiny islands clustered around. " +
+    "These are the 'Small End Islands'. The larger outer end islands are made up of 3 different biomes each. There's the 'End Barrens' which is the edges of the larger islands. " +
+    "There's the 'End Midlands', which is the slope from the hillier parts of a larger island to the edges of it. And finally there's the 'End Highlands', the hilltops of the larger islands. " +
+    "The 'End Highlands' is the only place chorus fruit and end cities spawn.</p>";
+    document.getElementById("f").style.height = "auto";
 }
+netherFunction();
--- JavaScript/structures.js
+++ JavaScript/structures.js
@@ -1,35 +1,66 @@
 function overworldFunction() {
     document.getElementById("info").innerHTML = 
-        /* Heading */"<h1>Overworld</h1>There are many structures in the overworld. This page will cover mostly beginner-friendly and later-game Overworld structures" +
+        /* Heading */"<h1>Overworld</h1>There are many structures in the overworld. This page will cover mostly beginner-friendly and later-game Overworld structures." +
         /* Ancient city */"<h3>Ancient City</h3><img src ='/Images/ancientCity.jpg' alt='ancient city' style='float:right;'>" +
         "Ancient Cities spawn around Y-level -52 in the deep dark biome. No mobs spawn withing the perimeter of an ancient city, even though "+ 
         "the structure has very minimal lighting. Dotted around ancient cities and nearby cave systems are patches of sculk, sculk sensors and sculk shriekers. " +
         "<p>Ancient cities can be for any point in the game provided a player has sufficient ways to keep their actions quiet. " +
         "If the shriekers are activated 3 or more times, they spawn a warden. The only way to not activate the sensors or shriekers is to crouch near them or walk on wool blocks or carpets." +
         "It is recommend but not necessary to bring either totems of undying or instant health potions.</p>" +
-        "<p>Notable loot includes the ward and silence trims, enchanted golden apples, coal, bones, regeneration potions, all enchantment books barring wind burst and soul speed, " +
-        "diamond pants, diamond horse armor, name tags, saddles, otherside music disks, echo shards, sculk, sculk sensors and sculk catalysts.</p>"+
-        /* Desert Temple */"<h3>Desert Temple</h3><img src ='/Images/desertTemple.jpg' alidden rooms in the ground floor of the structure. " +
-        "The first one is hidden under the terracotta pattern on the floor. It is bett='desert temple' style='float:left;'>" +
-        "Desert temples are a very easy and good structure for early game. There are 2 hter to dig down a side than fall down, as there is a pressure plate that if activated, " +
+        "<p>Notable loot includes the ward and silence armor trims, enchanted golden apples, coal, bones, regeneration potions, all enchantment books barring wind burst and soul speed, " +
+        "diamond pants, diamond horse armor, name tags, saddles, otherside music disks, echo shards, sculk, sculk sensors and sculk catalysts.</p><br>"+
+        /* Desert Temple */"<h3>Desert Temple</h3><img src ='/Images/desertTemple.jpg' alt='desert temple' style='float:left;'>" +
+        "Desert temples are a very easy and good structure for early game. There are 2 hidden rooms in the ground floor of the structure. " +
+        "The first one is hidden under the terracotta pattern on the floor. It is better to dig down a side than fall down, as there is a pressure plate that if activated, " +
         "will trigger 9 pieces of TNT and blow up all the chests. The second secret room is flooded with sand and contains no chests to loot, the archaeological loot is in the suspicous sand. " +
         "The sand in the floor of the ground floor is the ceiling of the sand-filled secret room."+
         "<p>The most notable chest loot are gunpowder, bones, string, saddles, gold, iron, diamonds, emeralds, golden apples, enchanted golden apples, diamond horse armor, gold horse armor, " +
-        "iron horse armor, dune trim and all enchanting books excluding swift sneak, soul speed and wind burst.</p>" +
-        "<p>Notable archaeological loot in the desert temple are gunpowder, diamonds, emeralds, TNT, and archer, miner, prize and skull pottery sherds.</p>" +
+        "iron horse armor, dune armor trim and all enchanting books excluding swift sneak, soul speed and wind burst.</p>" +
+        "<p>Notable archaeological loot in the desert temple are gunpowder, diamonds, emeralds, TNT, and archer, miner, prize and skull pottery sherds.</p><br><br>" +
         /* Shipwreck */"<h3>Shipwreck</h3><img src ='/Images/shipwreck.webp' alt='shipwreck' style='float:right;'>" +
         "The shipwreak is another very easy structure for early game. It can either spawn underwater or more rarely, beached on a iceburg or beach. It can also appear as part of a ship or a full ship. " +
         "Shipwrecks generate in all ocean biomes, drowned might alo spawn be around the ship." +
         "<p>There are 3 chests on a shipwreck if it is fully intact.</p><p>The first is in the captain's quarters and its notable loot is iron, gold, lapis lazuli, diamonds, bottles o' enchanting and coast trim.</p>" +
         "The other 2 chests will be below deck, one closer to the stern of the ship and the other closer to the bow of the ship. " +
-        "<p>Notable loot from the chest under the deck by the stern is a buried treasure map, books, paper, feathers, empty maps and coast trim.</p>" +
+        "<p>Notable loot from the chest under the deck by the stern is a buried treasure map, books, paper, feathers, empty maps and coast armor trim.</p>" +
         "<p>Finally, the notable loot from the chest by the ship's bow unde the bow is paper, wheat, carrots, potatoes, bamboo, gunpowder, TNT, coal, moss blocks, lether armor pieces that could be enchanted and coast trim.</p>" +
-        "<br><br><br><br><br><br><br><br>"+/* Piller outpost */"<br>pillager outpost" +
-        /* Stronghold */"stronghold" +
-        /* Trial Chambers */"<br>trial chambers" +
-        /* Trail Ruins */"<br>trail ruins" +
-        /* Village */"<br>village" +
-        /* Woodland Mansion */"<br>woodland mansion";
+        /* Piller outpost */"<h3>Pillager Outpost</h3><img src ='/Images/pillagerOutpost.webp' alt='pillager outpost' style='float:left;'>" +
+        "<p>Pillager outposts spawn in a variety of biomes, including deserts, plains, meadows, groves, snowy slopes, jagged peaks, stony peaks, savannas, snowy plains, taigas and cherry groves." +
+        "There are a few different mini structures that can generate with the main tower. One mini structure are some cages that will either have iron golems or allays. " +
+        "The mini structure can also be a pile of dark oak logs, some scarecrows for target practice or some white tents. " +
+        "Pillagers are the only hostile mobs that spawn when the player gets close, and they all usually spawn on the ground. So long as pillagers are able to keep line-of-sight, they will chase the play regardless of distance.</p>"+
+        "<p>This structure can be for early game, though if a player doesn't have a shield then it's recommended they run inside the structure and baracade the door with blocks.</p>" +
+        "<p>Notable loot from this structure includes wheat, carrots, potatoes, bottles o' enchanting, goat horns, arrows, crossbows, string, iron, enchanting books and sentry armor trim.<br><br><br>" +
+        /* Stronghold */"<h3>Stronghold</h3><img src ='/Images/stronghold.webp' alt='stronghold' style='float:right;'>" +
+        "<p>Strongholds generate underground in all biomes and can be located by throwing eyes of ender and following the direction they fly in. " +
+        "The stronghold structure can be done fairly easily in early game, it's recommended to bring torches and to have some marking system to keep track of where in the stronghold has been explored. " +
+        "Scattered around the stronghold are silverfish hiding in different stone blocks, so it's recommended to be careful breaking blocks, especially if they start to break quicker than normal.</p>" +
+        "<p>Somewhere in the stronghold is the frame of a portal to the End. When eyes of ender are placed within all the emtpy frames, the portal activates and is a one-way trip to the End. " +
+        "Going the the End and defeating the ender dragon is recommended to be done around middle to late game.</p>"+
+        "<p>Notable loot in the stronghold is coal, iron, gold, redstone, diamonds, ender pearls, iron armor, iron sword, iron pickaxe, eye armor trim, " +
+        "iron horse armor, gold horse armor, diamond horse armor, saddles, otherside music disk, books and enchanted books.</p><br><br>" +
+        /* Trial Chambers */"<h3>Trial Chambers</h3><img src ='/Images/trialChambers.webp' alt='pillager outpost' style='float:left;'>" +
+        "<p>Trial chambers spawn between Y-level 0 to -40 are recommended middle to late game and can be found via maps obtained from cartographer villager. They have vast amounts of copper and tuft bricks. " +
+        "Scattered throughout the trial chambers are trial spawners that spawn mobs based on the amount of nearby players. " +
+        "After all mobs from the spawner have been defeated, the spawner spits out loot from the top. This can include trial keys and ominous trial keys if a player has the bad omen effect. " +
+        "There are several different mobs that can be spawned from trial spawners, including breezes, slimes, bogged, strays, skeletons, husks, zombies, spiders, cave spiders and silverfish.</p>" +
+        "<p>There is a vast amount of places for loot to be hidden in, such as chests, barrels, decorated pots, dispensers, trial spawners, vaults ominous spawners and ominous vaults. " +
+        "Ominous spawners are normal trial spawners that have been activated by a player with the ominous effect. Ominous vaults are a different vault than regualr vaults that have a blue strip at the bottom of the and have 3 faces with red eyes on the front face of the vault block. " +
+        "Regular vaults have an orange strip at the bottom and one face with orange eyes on the front face of the vault block</p>" +
+        "<p>Notable loot in the chests, barrels, decorated pots and dispensers includes arrows, trial keys, honeycomb, cake, iron, blocks of iron, diamonds, blocks of diamond, blocks of emerald, " +
+        "damaged diamond axe, damaged diamond pickaxes, milk buckets, strength potions, regeration potions, glow berries, poison arrows, slowness arrows, scaffolding, ender pearls, creator music disk, " +
+        "water buckets, healing lingering potion, poison lingering potion, slowness lingering potion, weakness lingering potion, poison splash potion, slowness splash potion and weakness splash potion.</p>" +
+        "<p>Notable loot from regular spawners is trail key, regeneration potion and swiftness potion. Notable loot from ominous spawners is ominous trial key, regeneration potion, strength potion and golden carrots.</p>" +
+        "Notable loot from regular vaults is iron, diamonds, emeralds, golden carrots, golden apples, wind charges, enchanted books, bolt armor trim, precipice music disk, enchanted diamond chestplate, enchanted diamond axe and ominous bottle." +
+        "Finally, notable loot from ominous vaults includes diamonds, emeralds, flow armor trim, flow banner pattern, blocks of diamond, blocks of iron, blocks of emerald, heavy core, enchanted books, ominous bottle and enchanted golden apple."+
+        /* Village */"<h3>Village</h3><img src ='/Images/village.webp' alt='village' style='float:right;margin: 0px 15px 50px 15px'> "+
+        "<p>Villages are great early game structures because there are no hostile mobs that spawn when the chuncks of a village are loaded. Vilages can spawn in snowy plains, taigas, meadows, savannas, plains and deserts"+
+        "They are the only structures to vary based on biome they're in. The buildings in villages will be made out of whatever wood type is the main tree that spawns in the biome it generated in. " +
+        "For villages that generate in deserts, their buildings are made out of different cuts of sandstone. All villages have a few plots of fully grown crops when they generate, along with a few cats and an iron golem or two. " +
+        "Villagers also spawn when a village is first generated and players can trade different items based on a villager's profession along with emeralds to get a variety of things.</p>"+
+        "<p>Village loot depends on what buildings spawn, so a generalised list of good loot includes coal, iron, redstone, lapis lazuli, gold, diamonds, emeralds, leather, bundle, " +
+        "leather armor, iron armor, iron sword, iron pickaxe, all horse armor excluding leather horse armor, saddle, wheat, bread, shears, wool, arrowns, feathers, maps, paper and saplings varying based on the biome the village generated in.</p>"
+        ;
         document.getElementById("f").style.height = "auto";
 }
 
