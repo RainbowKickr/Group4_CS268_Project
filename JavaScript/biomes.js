3af1a3e224bf86efcb85a651a09012789d5143a4
stormingsilver <155936140+stormingsilver@users.noreply.github.com>
2025-05-04 23:21:14 -0500

    Final touches on my pages

--- JavaScript/biomes.js
+++ JavaScript/biomes.js
@@ -1,7 +1,7 @@
 function overworldFunction() {
     document.getElementById("info").innerHTML= 
     /* Intro */"There are 54 different biomes in the Overworld compared to the Nether's and End's 5 biomes each, so biomes are grouped generally by biome type. Any specific information for a biome includes their name." +
-    /* Forest Biomes */"<h3>Forest Biomes</h3><img src=/Images/paleGarden.png alt='pale garden, dark oak forest and jungle' style='float:right'>" +
+    /* Forest Biomes */"<h3>Forest Biomes</h3><img src=./Images/paleGarden.png alt='pale garden, dark oak forest and jungle' style='float:right'>" +
     "<p>The trees with the gray leaves in the picture to the right is a pale garden. Pale oak trees are only found in this biome, along with pale moss, hanging moss and eyeblossoms. "+
     "At night creakings spawn and attack any player not looking at them.</p>"+
     "Immediately surrounding the pale garden is a dark oak forest. These trees are also only found in this biome. " +
@@ -14,7 +14,7 @@ function overworldFunction() {
     "While flowers can generate here, there will be more flowers and more varities of flowers in the flower grove. " +
     "Finally, in all of the taiga biomes, spruce trees will be the only tree to generate. " +
     "Only grass will generate on the ground in normal taigas, whereas in old growth taigas it will be a mix between grass, podzl and sometimes corse dirt.</p>" +
-    /* Mountainous Biomes */"<h3>Mountainous biomes</h3><img src=/Images/cherryGrove.png alt='cherry grove' style='float:left'>" +
+    /* Mountainous Biomes */"<h3>Mountainous biomes</h3><img src=./Images/cherryGrove.png alt='cherry grove' style='float:left'>" +
     "<p>Pictured to the left is a cherry grove. This is the only place cherry trees and pink petals spawn. You can also find patches of poppies and dandelions." +
     "There are a few other mountainous biomes that don't have snow, including meadows, windswept hills, windswept gravelly hills, windswept forest and stony peaks." +
     "Meadows are located on top of mountains, have barely any trees and a variety of flowers. Windswept hills are similar, but with less flowers. " +
@@ -25,7 +25,7 @@ function overworldFunction() {
     "Grove is the only snowy mountain biome to have trees, spruce trees specifically, all other snowy mountain biomes don't have any trees. "+
     "Snowy slopes have mountains with softer summits than the jagged peaks biome. Snowy slopes and grove are the only 2 biomes to generate powdered snow." +
     "Jagged peaks and frozen peaks both have sharper and harder-to-traverse mountains, though the main difference between the two biomes is that frozen peaks also naturally generates with large patches of ice while jagged peaks does not.</p>"+
-    /* Desert & Savanna Biomes */"<h3>Desert & Savanna Biomes</h3><img src=/Images/desertBadlandsAndSavanna.avif alt='mangrove swamp' style='float:right'>" +
+    /* Desert & Savanna Biomes */"<h3>Desert & Savanna Biomes</h3><img src=./Images/desertBadlandsAndSavanna.avif alt='mangrove swamp' style='float:right'>" +
     "<p>The desert biome is made up of entirely regular, yellow sand on the surface, with a few layers of sandstone below it before the regular stone. Scattered around on top of the sand are dead bushes, cacti and dry grass. " +
     "Villages that spawn in deserts will spawn with camels as well. The only trees that can be found in this biome are azalea trees if there is a lush cave beneth the sand.</p>" +
     "<p>The badlands and eroded badlands contain orange stand and orange standstone in place of yellow sand and sandstone. " +
@@ -33,13 +33,13 @@ function overworldFunction() {
     "And in both of these biomes, gold is sigifcantly more abundent and can spawn much higher up than in other biomes.</p>"+
     "<p>In the savanna, windswept savanna and savanna plateau, the ground is grass or dirt with acacia (a-kay-sh-a) tree scattered about sparsely. " +
     "A few flower pactches can be found, but no foliage from the desert or badland biomes will be found in savannas</p>"+
-    /* Swamp & Beach Biomes */"<h3>Swamp & Beach Biomes</h3><img src=/Images/mangroveSwamp.avif alt='mangrove swamp' style='float:left'>" +
+    /* Swamp & Beach Biomes */"<h3>Swamp & Beach Biomes</h3><img src=./Images/mangroveSwamp.avif alt='mangrove swamp' style='float:left'>" +
     "<p>Pictured to the left is a mangrove swamp. This and a normal swamp are partially water, partially land. One  difference between the two is that mangrove swamps are the only place mud naturally generates. "+
     "A variation of normal oak trees generate across normal swamp biomes while large mangrove trees generate in mangrove forests. Witch huts can generate in both biomes, and frogs can also spawn in both biomes.</p>"+
     "<p>There are 3 diffrerent beach-like biomes. First is the regular beach biome that generates with sand, rarely a player may find a beached shipwreck here. The second kind of beach is the stony shore biome." +
     "These shores are made up of stone with occasional patches of gravel and dirt, with small clumps of coal and sometimes iron. These can generate right next to mountains so their slopes can vary drasticly compared to both other types of beaches." +
     "The final beach biome is the snowy beach. It is almost identical to the regular beach, but on top of the sand there's a layer or two of snow</p>One final note is that no passive mobs spawn on any of these beaches."+
-    /* Ocean Biomes */"<img src=/Images/oceanIsOcean.jpg alt='mr. incredibles 'ocean is ocean' meme' style='float:right;width:200px;height:auto;margin: 0px 15px 50px 15px'><h3>Ocean Biomes</h3>"+
+    /* Ocean Biomes */"<img src=./Images/oceanIsOcean.jpg alt='mr. incredibles 'ocean is ocean' meme' style='float:right;width:200px;height:auto;margin: 0px 15px 50px 15px'><h3>Ocean Biomes</h3>"+
     "<p>There are 9 different ocean biomes: warm ocean, lukewarm ocean, deep lukewarm ocean, ocean, deep ocean, cold ocean, deep cold ocean, frozen ocean and deep cold frozen ocean. " +
     "2 depths for almost all oceans, regular and deep, and 5 different temperatures for all oceans, warm, lukewarm, regular, cold and frozen. " +
     "The major different between deep oceans and regular oceans is that ocean monuemnts can spawn in deep oceans because of the extra depth, whereas they don't spawn in an ocean with a regular depth. "+ 
@@ -52,14 +52,14 @@ function overworldFunction() {
 function netherFunction() {
     document.getElementById("info").innerHTML= 
     /* Intro */"<p>There are 5 biomes in the Nether, each very unique from the others. Below each biome are lava lakes that go to bedrock.</p>" +
-    /* Basalt Delta */"<h3>Basalt Delta</h3><img src='/Images/basaltdelta.webp' alt='basalt delta' style=float:left>" +
+    /* Basalt Delta */"<h3>Basalt Delta</h3><img src='./Images/basaltdelta.webp' alt='basalt delta' style=float:left>" +
     "<p>The first one to mention is the 'Basalt Deltas'. Made up of mostly its namesake, basalt in varying heights of pillers. " +
     "There is also a good amount of blackstone, magma blocks and lava scattered throughtout the basalt deltas.</p>" +
     "<p>Quartz ore, neather gold ore and ancient debris all spawn underground in basalt deltas</p>" +
     "<p>Be wary while traversing through here, attempting to mine directly through the basalt could lead to lava pools. " +
     "Traveling through these biomes is recommended to be done with an elytra or by placing blocks.</p><br>" +
     "<p>Magma cubes spawn more frequently in basalt deltas, striders and ghasts also spawn in basalt deltas. Pigmen, piglins and hoglins don't spawn in basalt deltas.</p>" +
-    /* Crimson Forest */"<h3>Crimson Forest</h3><img src='/Images/crimsonForest.webp' alt='crimson forest' style=float:right>" +
+    /* Crimson Forest */"<h3>Crimson Forest</h3><img src='./Images/crimsonForest.webp' alt='crimson forest' style=float:right>" +
     "Crimson forests house grown crimson fungi, which are the tree-like plants with red warts for leaves and crimson stems for trunks. " +
     "Hung on the interior of the red warts are shroomlights, and weeping vines hanging from the exterior." +
     "Crimson fungi and occasionally warped fungi can be found on the crimson nylium-covered netherrack. Nither will grow unless bonemealed." +
@@ -67,7 +67,7 @@ function netherFunction() {
     "<p>When traveling by foot through a crimson forest, it's recommeded you bring a warped fungis to scare off hoglins. " +
     "A piece of gold armor is also recommended in case a player runs into any piglins.</p>"+
     "<p>The mobs that can spawn here are striders, hoglins, piglins and zombified piglins while all other nether mobs don't spawn here.</p><br>" +
-    /* Warped Forest */"<h3>Warped Forest</h3><img src='/Images/warpedForest.webp' alt='warped forest' style=float:left>" +
+    /* Warped Forest */"<h3>Warped Forest</h3><img src='./Images/warpedForest.webp' alt='warped forest' style=float:left>" +
     "The warped forest is very similar to crimson forests, one key difference is that instead of crimson nylium, crimson stems, crimson warts and weeping vines " +
     "there is warped nylium, warped stems, warped warts and twisting vines. Warped fungis is found much more frequently on the ground than crimson fungis, and it will not grow unless it's bonemealed." +
     "<p>Quartz ore and neather gold ore also spawn on cliffs and visable netherrack on the sides of hills in warped forests.</p>" +
@@ -75,22 +75,26 @@ function netherFunction() {
     "It is recommend to bring blocks for traversal of the biome and to aid in keeping hostile enderman away. " +
     "A carved pumpkin is something a player could wear if they don't want to worry about enderaman." +
     "<p>Another key difference is that absolutely no mobs except enderman spawn in warped forests. Hoglings will also actively avoid entering these forests.</p><br>" +
-    /* Nether Wastes */"<h3>Nether Wastes</h3><img src='/Images/netherWastes.webp' alt='nether wastes' style=float:right>" +
-    /* Soul Sand Valley */"<br><br><br><br><br><br><br><br><br><h3>Soul Sand Valley</h3><img src='/Images/soulSandValley.webp' alt='soul sand valley' style=float:left>";
+    /* Nether Wastes */"<h3>Nether Wastes</h3><img src='./Images/netherWastes.webp' alt='nether wastes' style=float:right>" +
+    "<p>Nether wastes are the most common nether biome and consist almost entirely of netherrack.</p><p>Nether gold ore and quartz ore can be found exposed on the surface, ancient debris is found under the surface.</p>" +
+    "<p>All nether mobs barring piglin brutes, blazes, wither skeletons and skeletons can spawn here.</p><br><br><br><br><br><br>" +
+    /* Soul Sand Valley */"<h3>Soul Sand Valley</h3><img src='./Images/soulSandValley.webp' alt='soul sand valley' style='float:left;margin: 0px 15px 50px 15px'>"+
+    "<p>Soul sand valleys are made up of soul sand and soul soil with the occasional bone block structure.</p>"+
+    "<p>No ore spawns directly on the surface of soul sand valleys, it is either on the side of a mountains and pillars or underground.</p><p>Skeletons spawn frequently here, but no other mob spawns here.</p>";
     document.getElementById("f").style.height = "auto";
 }
 
 function endFunction() {
     document.getElementById("info").innerHTML= "<p>There surprisingly are 5 biomes in the End, though all End biomes are made up of floating endstone islands.</p>"+
-    /* End */"<h3>End (the biome)</h3><img src='/Images/endBiome.webp' alt='end' (the biome) style='float:left;margin:0px 15px 15px 15px'>" +
+    /* End */"<h3>End (the biome)</h3><img src='./Images/endBiome.webp' alt='end' (the biome) style='float:left;margin:0px 15px 15px 15px'>" +
     "<p>The first one you encounter is called 'The End'. It's the end island a player spawn on. The radius of the end island is roughly 1,000 blocks (meters) and is centered at (0, 0). " +
     "Large obsidian pillers with end crystals generate in a circle formation around the end portal back to the overworld. " +
     "The first time a player enters the ender dragon spawns and the portal home is not activated until the ender dragon is defeated.</p>" +
-    /* All other biomes */"</h3><img src='/Images/endAllOtherBiomes.png' alt= 'all other end biomes' style='float:right;margin: 0px 15px 50px 15px'>" +
+    /* All other biomes */"</h3><img src='./Images/endAllOtherBiomes.png' alt= 'all other end biomes' style='float:right;margin: 0px 15px 50px 15px'>" +
     "<h3>Small End Islands, End Midlands, End Highlands, End Barrens</h3><p>After using the end gateway to teleport to the further away islands, you will notice there are some very tiny islands clustered around. " +
     "These are the 'Small End Islands'. The larger outer end islands are made up of 3 different biomes each. There's the 'End Barrens' which is the edges of the larger islands. " +
     "There's the 'End Midlands', which is the slope from the hillier parts of a larger island to the edges of it. And finally there's the 'End Highlands', the hilltops of the larger islands. " +
     "The 'End Highlands' is the only place chorus fruit and end cities spawn.</p>";
     document.getElementById("f").style.height = "auto";
 }
-overworldFunction();
+netherFunction();
--- JavaScript/structures.js
+++ JavaScript/structures.js
@@ -1,7 +1,7 @@
 function overworldFunction() {
     document.getElementById("info").innerHTML = 
         /* Heading */"<h1>Overworld</h1>There are many structures in the overworld. This page will cover mostly beginner-friendly and later-game Overworld structures." +
-        /* Ancient city */"<h3>Ancient City</h3><img src ='/Images/ancientCity.jpg' alt='ancient city' style='float:right;'>" +
+        /* Ancient city */"<h3>Ancient City</h3><img src ='./Images/ancientCity.jpg' alt='ancient city' style='float:right;'>" +
         "Ancient Cities spawn around Y-level -52 in the deep dark biome. No mobs spawn withing the perimeter of an ancient city, even though "+ 
         "the structure has very minimal lighting. Dotted around ancient cities and nearby cave systems are patches of sculk, sculk sensors and sculk shriekers. " +
         "<p>Ancient cities can be for any point in the game provided a player has sufficient ways to keep their actions quiet. " +
@@ -9,7 +9,7 @@ function overworldFunction() {
         "It is recommend but not necessary to bring either totems of undying or instant health potions.</p>" +
         "<p>Notable loot includes the ward and silence armor trims, enchanted golden apples, coal, bones, regeneration potions, all enchantment books barring wind burst and soul speed, " +
         "diamond pants, diamond horse armor, name tags, saddles, otherside music disks, echo shards, sculk, sculk sensors and sculk catalysts.</p><br>"+
-        /* Desert Temple */"<h3>Desert Temple</h3><img src ='/Images/desertTemple.jpg' alt='desert temple' style='float:left;'>" +
+        /* Desert Temple */"<h3>Desert Temple</h3><img src ='./Images/desertTemple.jpg' alt='desert temple' style='float:left;'>" +
         "Desert temples are a very easy and good structure for early game. There are 2 hidden rooms in the ground floor of the structure. " +
         "The first one is hidden under the terracotta pattern on the floor. It is better to dig down a side than fall down, as there is a pressure plate that if activated, " +
         "will trigger 9 pieces of TNT and blow up all the chests. The second secret room is flooded with sand and contains no chests to loot, the archaeological loot is in the suspicous sand. " +
@@ -17,21 +17,21 @@ function overworldFunction() {
         "<p>The most notable chest loot are gunpowder, bones, string, saddles, gold, iron, diamonds, emeralds, golden apples, enchanted golden apples, diamond horse armor, gold horse armor, " +
         "iron horse armor, dune armor trim and all enchanting books excluding swift sneak, soul speed and wind burst.</p>" +
         "<p>Notable archaeological loot in the desert temple are gunpowder, diamonds, emeralds, TNT, and archer, miner, prize and skull pottery sherds.</p><br><br>" +
-        /* Shipwreck */"<h3>Shipwreck</h3><img src ='/Images/shipwreck.webp' alt='shipwreck' style='float:right;'>" +
+        /* Shipwreck */"<h3>Shipwreck</h3><img src ='./Images/shipwreck.webp' alt='shipwreck' style='float:right;'>" +
         "The shipwreak is another very easy structure for early game. It can either spawn underwater or more rarely, beached on a iceburg or beach. It can also appear as part of a ship or a full ship. " +
         "Shipwrecks generate in all ocean biomes, drowned might alo spawn be around the ship." +
         "<p>There are 3 chests on a shipwreck if it is fully intact.</p><p>The first is in the captain's quarters and its notable loot is iron, gold, lapis lazuli, diamonds, bottles o' enchanting and coast trim.</p>" +
         "The other 2 chests will be below deck, one closer to the stern of the ship and the other closer to the bow of the ship. " +
         "<p>Notable loot from the chest under the deck by the stern is a buried treasure map, books, paper, feathers, empty maps and coast armor trim.</p>" +
         "<p>Finally, the notable loot from the chest by the ship's bow unde the bow is paper, wheat, carrots, potatoes, bamboo, gunpowder, TNT, coal, moss blocks, lether armor pieces that could be enchanted and coast trim.</p>" +
-        /* Piller outpost */"<h3>Pillager Outpost</h3><img src ='/Images/pillagerOutpost.webp' alt='pillager outpost' style='float:left;'>" +
+        /* Piller outpost */"<h3>Pillager Outpost</h3><img src ='./Images/pillagerOutpost.webp' alt='pillager outpost' style='float:left;'>" +
         "<p>Pillager outposts spawn in a variety of biomes, including deserts, plains, meadows, groves, snowy slopes, jagged peaks, stony peaks, savannas, snowy plains, taigas and cherry groves." +
         "There are a few different mini structures that can generate with the main tower. One mini structure are some cages that will either have iron golems or allays. " +
         "The mini structure can also be a pile of dark oak logs, some scarecrows for target practice or some white tents. " +
         "Pillagers are the only hostile mobs that spawn when the player gets close, and they all usually spawn on the ground. So long as pillagers are able to keep line-of-sight, they will chase the play regardless of distance.</p>"+
         "<p>This structure can be for early game, though if a player doesn't have a shield then it's recommended they run inside the structure and baracade the door with blocks.</p>" +
         "<p>Notable loot from this structure includes wheat, carrots, potatoes, bottles o' enchanting, goat horns, arrows, crossbows, string, iron, enchanting books and sentry armor trim.<br><br><br>" +
-        /* Stronghold */"<h3>Stronghold</h3><img src ='/Images/stronghold.webp' alt='stronghold' style='float:right;'>" +
+        /* Stronghold */"<h3>Stronghold</h3><img src ='./Images/stronghold.webp' alt='stronghold' style='float:right;'>" +
         "<p>Strongholds generate underground in all biomes and can be located by throwing eyes of ender and following the direction they fly in. " +
         "The stronghold structure can be done fairly easily in early game, it's recommended to bring torches and to have some marking system to keep track of where in the stronghold has been explored. " +
         "Scattered around the stronghold are silverfish hiding in different stone blocks, so it's recommended to be careful breaking blocks, especially if they start to break quicker than normal.</p>" +
@@ -39,7 +39,7 @@ function overworldFunction() {
         "Going the the End and defeating the ender dragon is recommended to be done around middle to late game.</p>"+
         "<p>Notable loot in the stronghold is coal, iron, gold, redstone, diamonds, ender pearls, iron armor, iron sword, iron pickaxe, eye armor trim, " +
         "iron horse armor, gold horse armor, diamond horse armor, saddles, otherside music disk, books and enchanted books.</p><br><br>" +
-        /* Trial Chambers */"<h3>Trial Chambers</h3><img src ='/Images/trialChambers.webp' alt='pillager outpost' style='float:left;'>" +
+        /* Trial Chambers */"<h3>Trial Chambers</h3><img src ='./Images/trialChambers.webp' alt='pillager outpost' style='float:left;'>" +
         "<p>Trial chambers spawn between Y-level 0 to -40 are recommended middle to late game and can be found via maps obtained from cartographer villager. They have vast amounts of copper and tuft bricks. " +
         "Scattered throughout the trial chambers are trial spawners that spawn mobs based on the amount of nearby players. " +
         "After all mobs from the spawner have been defeated, the spawner spits out loot from the top. This can include trial keys and ominous trial keys if a player has the bad omen effect. " +
@@ -53,7 +53,7 @@ function overworldFunction() {
         "<p>Notable loot from regular spawners is trail key, regeneration potion and swiftness potion. Notable loot from ominous spawners is ominous trial key, regeneration potion, strength potion and golden carrots.</p>" +
         "Notable loot from regular vaults is iron, diamonds, emeralds, golden carrots, golden apples, wind charges, enchanted books, bolt armor trim, precipice music disk, enchanted diamond chestplate, enchanted diamond axe and ominous bottle." +
         "Finally, notable loot from ominous vaults includes diamonds, emeralds, flow armor trim, flow banner pattern, blocks of diamond, blocks of iron, blocks of emerald, heavy core, enchanted books, ominous bottle and enchanted golden apple."+
-        /* Village */"<h3>Village</h3><img src ='/Images/village.webp' alt='village' style='float:right;margin: 0px 15px 50px 15px'> "+
+        /* Village */"<h3>Village</h3><img src ='./Images/village.webp' alt='village' style='float:right;margin: 0px 15px 50px 15px'> "+
         "<p>Villages are great early game structures because there are no hostile mobs that spawn when the chuncks of a village are loaded. Vilages can spawn in snowy plains, taigas, meadows, savannas, plains and deserts"+
         "They are the only structures to vary based on biome they're in. The buildings in villages will be made out of whatever wood type is the main tree that spawns in the biome it generated in. " +
         "For villages that generate in deserts, their buildings are made out of different cuts of sandstone. All villages have a few plots of fully grown crops when they generate, along with a few cats and an iron golem or two. " +
@@ -67,32 +67,32 @@ function overworldFunction() {
 function netherFunction() {
     document.getElementById("info").innerHTML =
     /* Heading */ "<h1>Nether Structures</h1>" +
-    /* Bastion Remnent */"<h3>Bastion Remnent</h3><img src ='/Images/bastionRemnent.jpg' alt='bastion remnent' style='float:right;'>" +
+    /* Bastion Remnent */"<h3>Bastion Remnent</h3><img src ='./Images/bastionRemnent.jpg' alt='bastion remnent' style='float:right;'>" +
         "<p>Bastion remnants or 'bastions'are found in every nether biome except basalt deltas. They are full of piglins, which can be avoided if a player wears gold. " +
         "Piglin brutes and the occasional hoglin ignore gold and hit very hard, so it's recommended to bring a shield. If a player hits a piglin brute, " +
         "all other piglins will attack the player, so it's also recommended to try to drop piglin brutes in lava or kill them through other indirect ways. </p>" +
         "<p>The most notable loot from bastions are snout smithing templates, smithing upgrades, lodestones, pigset music disk, ancient debris, " +
         "ancient debris scrap, diamond armor, diamond tools and a lot of gold.</p>" +
-    /* Nether Fortress */"<br><h3>Nether Fortress</h3><img src='/Images/netherFortress.jpg' alt='nether fortress' style='float:left'><p>Nether Fortresses or 'fortresses' are found " +
+    /* Nether Fortress */"<br><h3>Nether Fortress</h3><img src='./Images/netherFortress.jpg' alt='nether fortress' style='float:left'><p>Nether Fortresses or 'fortresses' are found " +
         "in all nether biomes. Parts of fortresses can be very opened aired, so bringing a bow for ghasts, blazes and skeletons is strongly recommended. Shields are also recommended to block " +
         "the smaller ranged strikes from blazes and for close quarters with wither skeletons. A stack or two of blocks is also recommended for shorting floor-to-ceiling " +
         "height to prevent wither skeletons from getting in melee range</p>" +
         "<p>The most notable loot from fortresses is iron horse armor, gold horse armor, diamond horse armor, saddles, gold, diamonds and rib armor trim template</p>" +
-     /* Ruined Portal */"<br><h3>Ruined Portal (Nether)</h3><img src ='/Images/ruinedPortalNether.jpg' alt='ruined portal (nether)' style='float:right;'>" +
+     /* Ruined Portal */"<br><h3>Ruined Portal (Nether)</h3><img src ='./Images/ruinedPortalNether.jpg' alt='ruined portal (nether)' style='float:right;'>" +
         "<p>Ruined Portals in the nether spawn about as frequently as their overworld counterparts. The only difference between this structures between dimensions is " +
         "that in the Nether, the decorative frame is made up of blackstone, whereas Overworld ruined portals have a decorative frame made up of stone. " +
         "Ruined portals are the only structures that generate in more than one dimension, and they spawn in any biome in the nether. In air-pockets they spawn between Y-level 32-100 and in open air they spawn between Y-level 27-100. "+
         "There is a 5% chance a giant ruined portal can spawn instead of a small nether portal.</p>" +
         "<p>Notable loot from ruined portals is gold, golden apples, enchanted golden apples, lodestones, golden carrots, glistering melon slices and bells. " +
         "If iron or gold nuggets are rolled for loot, they spawn in groups of 9-18 and 4-24 respectfully instead of singularly.</p>" +
-    /* Nether Fossil */"<br><img src='/Images/netherFossil.jpg' alt='nether fossil' style='float:left;margin: 0px 15px 50px 15px'><h3>Nether Fossil</h3><p>Nether " +
+    /* Nether Fossil */"<br><img src='./Images/netherFossil.jpg' alt='nether fossil' style='float:left;margin: 0px 15px 50px 15px'><h3>Nether Fossil</h3><p>Nether " +
         "fossils only spawn in soul sand vallies and are solely comprised of bone blocks.</p>" +
         "<p>The only notable loot is the bone blocks from the fossil, and dried ghasts that will be added in a future update.</p>";
 }
 
 function endFunction() {
     document.getElementById("info").innerHTML = 
-        /* End City */"<h3>End City</h3><img src=/Images/endCity.avif alt='end city' style='float:left;width:550px;margin: 0px 15px 50px 15px'>" +
+        /* End City */"<h3>End City</h3><img src=./Images/endCity.avif alt='end city' style='float:left;width:550px;margin: 0px 15px 50px 15px'>" +
         "<p>There is only one structure in the End, End Cities. End cities are found on the larger, outer islands in the End. Shulkers are the only mob that spawn in end cities, " +
         "though enderman can teleport into end cities. So it's recommended you bring a shield to deflect levation crystals, a carved pumpkin to be used in the " +
         "presence of enderman and some form of way to save yourself from plummeting to your death</p><p>These cities can have enchanted diamond armor, tools and loose diamonds, emeralds, gold and iron. " +
